0caa1fa736369db29c798d211f2a382807f71a5e
Arlene George <aemg@live.com>
2019-08-13 22:39:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@
 def square_array(numbers)
     counter = 0   
   while numbers[counter]  do 
-  new_numbers numbers[counter]*numbers[counter]
+  new_numbers = numbers[counter]*numbers[counter]
     counter += 1
   end
   new_numbers
