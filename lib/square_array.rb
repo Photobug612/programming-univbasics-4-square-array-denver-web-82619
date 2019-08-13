a263e4964ac2050aaefb8ac462c2c74cbf11c7b7
Arlene George <aemg@live.com>
2019-08-13 22:40:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 
 def square_array(numbers)
+  squared = []
     counter = 0   
   while numbers[counter]  do 
   new_number = numbers[counter]*numbers[counter]
     counter += 1
+    new_number << squared 
   end
-  new_numbers
+  squared
 end
